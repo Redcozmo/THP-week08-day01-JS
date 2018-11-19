fbe3c0b0ee3879c82e3ea5b75f51f6f10945599b
Redcozmo <redcozmo@hotmail.com>
2018-11-19 15:44:35 +0100

    init exo calculatrice

---
+++ calculatrice.js
@@ -0,0 +1,22 @@
+// THP - Week 08 - Day 01 - Calculatrice
+// JavaScript
+
+// Somme
+function add(a, b){
+  return a + b;
+}
+
+// Soustraction
+function substract(a, b){
+  return a - b;
+}
+
+// Multiplication
+function multiply(a, b){
+  return a * b;
+}
+
+// Division
+function divide(a, b){
+  return a / b;
+}
